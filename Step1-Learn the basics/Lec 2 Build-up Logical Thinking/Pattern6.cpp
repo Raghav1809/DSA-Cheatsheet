c61e4de29659f3781fd5697e909932df24a0ba9c
Raghav1809 <dattawadkarraghav@gmail.com>
2026-06-18 10:47:24 +0530

    File Uploaded

---
+++ Step1-Learn the basics/Lec 2 Build-up Logical Thinking/Pattern6.cpp
@@ -0,0 +1,25 @@
+// 1
+// 1 0
+// 1 0 1
+// 1 0 1 0
+// 1 0 1 0 1
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i=1; i<=n; i++){
+
+        for(int j=1; j<=i; j++){
+            if(j % 2 == 0) {
+                cout << "0 ";
+            } else {
+                cout << "1 ";
+            }
+        }
+        cout << endl;
+    }
+}
