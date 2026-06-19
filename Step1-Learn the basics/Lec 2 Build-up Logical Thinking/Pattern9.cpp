4f1341b33fb0b63c61c97874db3ef60736ad493c
Raghav1809 <dattawadkarraghav@gmail.com>
2026-06-19 18:33:38 +0530

    File Uploaded

---
+++ Step1-Learn the basics/Lec 2 Build-up Logical Thinking/Pattern9.cpp
@@ -0,0 +1,29 @@
+// * * * * *
+//  * * * *
+//   * * * 
+//    * * 
+//     * 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        // Print leading spaces
+        for (int space = 0; space < i; space++) {
+            cout << " ";
+        }
+
+        // Print stars
+        for (int star = 0; star < n - i; star++) {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
